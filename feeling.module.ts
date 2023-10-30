ed5bd226ce073b2a4e3d8041dfe29af4afe7c572
projeta618 <projeta618@gmail.com>
2023-10-30 10:48:37 -0300

    Create the initial commit

---
+++ feeling.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LikeComponent } from './components/like/like.component';
+
+
+
+@NgModule({
+  declarations: [
+    LikeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FeelingModule { }
